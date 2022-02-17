caf17c03babf7044582bab9fefd00256a546f823
yyscolin <48078302+yyscolin@users.noreply.github.com>
2022-02-17 18:08:09 +1100

    Error handling added to update-handler function

--- polling.py
+++ polling.py
@@ -1,14 +1,10 @@
 import datetime
-import logging
 import os
-from pathlib import Path
 import telegram
 import time
-import traceback
 
 from modules.handler import handle_update
-import modules.mydb as mydb
-
+import modules.logger as logger
 
 telebot = telegram.Bot(token=os.getenv("bot_token"))
 is_error_logged = {
@@ -16,40 +12,26 @@ is_error_logged = {
 }
 
 
-def initialize_log_file():
-    log_folder = os.getenv("log_folder") or "logs"
-    Path(log_folder).mkdir(parents=True, exist_ok=True)
-    logging.basicConfig(
-        filename=os.path.join(log_folder, datetime.datetime.now().strftime("%Y-%m-%d") + ".log"),
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(funcName)s - line %(lineno)d"
-    )
-
-
-def run_polling():
-    timenow = datetime.datetime.utcnow()
-    for update_obj in telebot.get_updates(timeout=60):
-        handle_update(update_obj.to_dict(), timenow)
-
-
 def start_polling():
     telebot.deleteWebhook()
     is_looping = True
     run_interval = int(os.getenv("run_interval") or 0)
+    logger.initialize_log_file()
     while is_looping:
         has_internet_error = False
         try:
-            initialize_log_file()
-            run_polling()
+            telegram_updates = telebot.get_updates(timeout=60)
         except telegram.error.NetworkError:
             has_internet_error = True
             if is_error_logged["internet"] is False:
-                traceback.print_exc()
-                logging.exception("message")
+                logger.log_exception()
                 is_error_logged["internet"] = True
-            pass
         except:
-            traceback.print_exc()
-            logging.exception("message")
+            logger.log_exception()
+        else:
+            timenow = datetime.datetime.utcnow()
+            for update_obj in telegram_updates:
+                handle_update(update_obj.to_dict(), timenow)
 
         if is_error_logged["internet"] and has_internet_error is False:
             is_error_logged["internet"] = False
